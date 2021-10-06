5caa9b3e4693c3b61121175da49d78110b478c2d
Igor Cristiano Suffes <cristianosuqe@gmail.com>
2021-10-05 22:00:00 -0300

    Desafio conceitos React Native - Dia 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SafeAreaView,
@@ -10,9 +10,24 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from "./services/api";
+
 export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    await api.post(`repositories/${id}/like`);
+
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
   }
 
   return (
@@ -20,35 +35,77 @@ export default function App() {
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
         <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+          <FlatList
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+            data={repositories}
+            keyExtractor={repositorie => repositorie.id}
+            renderItem={({item: repositorie}) => (
+              
+                <Text style={styles.repository}>{repositorie.title}</Text>
+              
+            )
+              
+            }
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
+          />
+
+          <FlatList
+
+            data={repositories}
+            keyExtractor={repositorie => repositorie.id}
+            renderItem={({item: repositorie}) => (
+              <View style={styles.techsContainer}>
+                <Text style={styles.tech}>
+                  {repositorie.tech}
+                </Text>
+                <Text style={styles.tech}>
+                  {repositorie.tech}
+                </Text>
+              </View>
+            )
+              
+            }
+
+          />
+
+          <FlatList
+
+            data={repositories}
+            keyExtractor={repositorie => repositorie.id}
+            renderItem={({item: repositorie}) => (
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${repositorie.id}`}
+                >{repositorie.likes} curtidas</Text>
+              </View>
+            )
+              
+            }
+
+          />
+
+          <FlatList
+
+            data={repositories}
+            keyExtractor={repositorie => repositorie.id}
+            renderItem={({item: repositorie}) => (
+              <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleLikeRepository(repositorie.id)}
+              // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+              testID={`like-button-${repositorie.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            )
+              
+            }
+
+          />
+
         </View>
       </SafeAreaView>
     </>
